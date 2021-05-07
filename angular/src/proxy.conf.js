ab02f030ccc2fc381d064b33cfded7530123266a
Aremou <faizouaremou@gmail.com>
2021-05-07 11:38:24 +0200

    improve proxy configuration

--- angular/src/proxy.conf.js
+++ angular/src/proxy.conf.js
@@ -2,13 +2,7 @@ const PROXY_CONFIG = {
     "/": {
         "target": "http://localhost:3000",
         "secure": false,
-        "bypass": function(req, res, proxyOptions) {
-            if (req.headers.accept.indexOf("html") !== -1) {
-                console.log("Skipping proxy for browser request.");
-                return "/index.html";
-            }
-            req.headers["X-Custom-Header"] = "yes";
-        }
+        "logLevel": "debug"
     }
 }
 module.exports = PROXY_CONFIG;
